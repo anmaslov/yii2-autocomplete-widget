fc9723c6520c3ffaae2960fc7879eae3e9332ca2
anmaslov <lelick29@gmail.com>
2015-06-03 20:53:55 +0300

    fix test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,15 @@ error_reporting(-1);
 define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_DEBUG', true);
 
-require(__DIR__ . '/../../../autoload.php');
-require(__DIR__ . '/../../../yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tests', __DIR__);
 
 new \yii\console\Application([
     'id' => 'unit',
     'basePath' => __DIR__,
-    'vendorPath' => __DIR__ . '/../../../',
+    'vendorPath' => __DIR__ . '/../vendor',
     'components' => [
         'assetManager' => [
             'class' => 'tests\AssetManager',
